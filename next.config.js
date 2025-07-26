56fa799aa7d0bb553cc62113f55106c62dea1c14
Erick <erick.tadeo@hotmail.com>
2025-07-26 01:51:45 -0500

    fix: configura dominio de Supabase Storage para optimización de imágenes en Next.js

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@ module.exports = {
     // Warning: Esto permite que el build de producción termine aunque haya errores de ESLint.
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hveqfsdzisitbsvyjevj.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
 }; 
